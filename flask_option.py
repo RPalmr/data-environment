a07d7f4a3032e3d04f03531e3e60cbca93e56376
reece palmer <reecelpalmer@gmail.com>
2023-10-17 14:12:10 +0100

    Progress on 2 tests passing

--- flask_option.py
+++ flask_option.py
@@ -6,8 +6,10 @@ def start():
     """returns the right message"""
     flask_env = os.environ.get("FLASK_ENV")
 
-    if flask_env== "development":
+    if flask_env == "development":
         return "Starting in development mode..."
+    elif flask_env == "production":
+        return "Starting in production mode..."
 
 if __name__ == "__main__":
     print(start())
